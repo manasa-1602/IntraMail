cbe36520f5aa1eb4fedefe050c1439ab6e963a85
Manasa A <amanasa1602@gmail.com>
2026-05-01 20:15:54 +0530

    added signup and login api

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# temporary storage
+users = []
+
+# data model
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
 
 
 @app.get("/")
 def home():
     return {"message": "IntraMail backend running"}
+
+
+# SIGNUP
+@app.post("/signup")
+def signup(user: User):
+    users.append(user)
+    return {"message": "User registered successfully"}
+
+
+# LOGIN
+@app.post("/login")
+def login(user: User):
+    for u in users:
+        if u.email == user.email and u.password == user.password:
+            return {"status": "login success"}
+    return {"status": "invalid credentials"}
